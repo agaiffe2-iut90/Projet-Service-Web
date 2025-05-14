92e7dffd7c18b17ff63546232f3251dbfb054b56
agaiffe2-iut90 <anna.gaiffe@edu.univ-fcomte.fr>
2025-05-14 22:55:31 +0200

    data cleaning

--- routes/prout/config/db.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    HOST: process.env.DB_HOST || 'localhost',
-    USER: process.env.DB_USER || 'bdd_sw',
-    DB_PORT: process.env.DB_PORT || 5432,
-    PASSWORD: process.env.DB_PASSWORD || 'Shaco25',
-    DB: process.env.DB_NAME || 'ponnyhappy',
-    dialect: process.env.DB_DIALECT || 'postgres',
-};
