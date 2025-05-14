3ec4550e83180d7c485716343f5c54ea900adac6
agaiffe2-iut90 <anna.gaiffe@edu.univ-fcomte.fr>
2025-05-14 22:48:42 +0200

    maj projet

--- database/db.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('ponnyhappy', 'bdd_sw', 'Shaco25', {
-    host: 'localhost',
-    dialect: 'postgres',
-}
-)
-
-module.exports = sequelize;
